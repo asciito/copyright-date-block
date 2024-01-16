c243152f2c5b44b28354477ccd3bc26ddd720226
asciito <ayax.cordova@aydev.mx>
2024-01-16 08:06:33 -0600

    build: code for production usage

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8199cb9807b2f61e8c88');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6d5eeb53f184a7688891');
